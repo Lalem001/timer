10da296d5df07d1667fd2b9602e7777844505f87
Luis Aleman <Luis@toppatch.com>
2014-01-07 00:14:53 -0500

    Import timer and tests

---
+++ tests/unit/timer.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    'use strict';
+    module('Timer');
+    test('basic ops', 14, function () {
+        var Timer = window.Timer,
+            timer = new Timer(5000, 15000);
+
+        equal(timer._isStarted, false, 'New timer indicates it is not started');
+        equal(timer._isPaused, false, 'New timer indicates it is not paused');
+
+        ok(timer.start(), 'Start timer');
+        equal(timer._isStarted, true, 'New timer indicates it is started');
+        equal(timer._isPaused, false, 'New timer indicates it is not paused');
+
+        ok(timer.pause(), 'Pause timer');
+        equal(timer._isStarted, true, 'New timer indicates it is started');
+        equal(timer._isPaused, true, 'New timer indicates it is paused');
+
+        ok(timer.resume(), 'Resume timer');
+        equal(timer._isStarted, true, 'New timer indicates it is started');
+        equal(timer._isPaused, false, 'New timer indicates it is not paused');
+
+        ok(timer.stop(), 'Stop timer');
+        equal(timer._isStarted, false, 'New timer indicates it is not started');
+        equal(timer._isPaused, false, 'New timer indicates it is not paused');
+    });
+
+    asyncTest('events', 4, function () {
+        var eventCount = 0,
+            Timer = window.Timer,
+            timer = new Timer(5, 10);
+        timer.on('completed', function () {
+            ok(true, '"completed" event fired');
+            eventCount += 1;
+        });
+        timer.on('timedout', function () {
+            ok(true, '"timedout" event fired');
+            eventCount += 1;
+        });
+        timer.on('all', function (e) {
+            var event = 'all:' + e;
+            if (event === 'all:completed' || event === 'all:timedout') {
+                ok(true, '"' + event + '" event fired');
+            } else {
+                ok(false, 'unexpected event "' + event + '"');
+            }
+            eventCount += 1;
+            if (eventCount >= 4) {
+                start();
+            }
+        });
+        timer.start();
+    });
+
+    asyncTest('extension', 4, function () {
+        var min = 50, max = 100, extend = 20,
+            tolerance = 5,
+            Timer = window.Timer,
+            timer = new Timer(min, max);
+        timer.on('completed', function () {
+            ok(
+                Math.abs(timer.elapsed() - (min + extend)) <= tolerance,
+                ['Completed at ', (min + extend), '±', tolerance, 'ms'].join('')
+            );
+        });
+        timer.on('timedout', function () {
+            ok(
+                Math.abs(timer.elapsed() - max) <= tolerance,
+                ['Timedout at ', max, '±', tolerance, 'ms'].join('')
+            );
+            start();
+        });
+        ok(timer.start(), ['Starting ', min, '/', max, ' Timer'].join(''));
+        ok(timer.extend(extend), 'Extend timer by ' + extend + 'ms');
+    });
+});
---
+++ timer.js
@@ -0,0 +1,163 @@
+(
+    function (factory) {
+        if (typeof define === 'function' && define.amd) {
+            // AMD. Register as an anonymous module.
+            define(['underscore', 'backbone'], factory);
+        } else if (typeof exports === 'object') {
+            module.exports = factory(require('underscore'), require('backbone'));
+        } else {
+            // Browser globals (root is window)
+            window.Timer = factory(window._, window.Backbone);
+        }
+    }(function (_, Backbone) {
+        /** Safe `undefined` reference */
+        var undefined;
+
+        var Timer;
+
+        // Timer
+        // Inspired by the timer logic used in Ubuntu's NotifyOSD
+        // --------------------------------------------------------------------------------
+        Timer = function (scheduledDuration, maxDuration) {
+            if (_.isUndefined(scheduledDuration) || !_.isNumber(scheduledDuration)) {
+                throw new TypeError('Timer: Scheduled duration is required and must be a number');
+            }
+            if (_.isUndefined(maxDuration) || !_.isNumber(maxDuration)) {
+                throw new TypeError('Timer: Max duration is required and must be a number');
+            }
+            if (scheduledDuration > maxDuration) {
+                throw new Error('Scheduled duration should be less than the max duration.');
+            }
+            this._pausedDuration = 0;
+            this._scheduledDuration = scheduledDuration;
+            this._maxDuration = maxDuration;
+            this._isStarted = false;
+            this._isPaused = false;
+
+            return this;
+        };
+
+        _.extend(Timer, {
+            getNow: Date.now || function () { return +(new Date()); }
+        });
+
+        _.extend(Timer.prototype, Backbone.Events, {
+            start: function () {
+                if (this._isStarted) { return false; }
+                var that = this;
+                this.scheduledTimer = this.setTimeout(function () {
+                    return that.emitCompleted();
+                }, this._scheduledDuration);
+                this.timedOutTimer = this.setTimeout(function () {
+                    that.stop();
+                    return that.emitTimeOut();
+                }, this._maxDuration);
+                this._isStarted = true;
+                this._startTime = Timer.getNow();
+                this._stopTime = undefined;
+                return true;
+            },
+            stop: function () {
+                if (!this._isStarted) { return false; }
+                this.scheduledTimer.clear();
+                this.timedOutTimer.clear();
+                this._isStarted = false;
+                this._isPaused = false;
+                this._stopTime = Timer.getNow();
+                return true;
+            },
+            pause: function () {
+                if (!this._isStarted || this._isPaused) { return false; }
+                this._isPaused = true;
+                this.scheduledTimer.clear();
+                this._pausedTime = Timer.getNow();
+                return true;
+            },
+            resume: function () {
+                if (!this._isStarted || !this._isPaused) { return false; }
+                this._isPaused = false;
+                this._pausedDuration += Timer.getNow() - this._pausedTime;
+                this._pausedTime = undefined;
+                var that = this,
+                    extension = this._scheduledDuration - this.scheduledTimer.elapsed();
+                this.scheduledTimer = this.setTimeout(function () { return that.emitCompleted(); }, extension);
+
+                return true;
+            },
+            extend: function (extension) {
+                if (!this._isStarted) { return false; }
+                if (this._isPaused) {
+                    if (this._scheduledDuration + extension > this._maxDuration) {
+                        this._scheduledDuration = this._maxDuration;
+                    } else {
+                        this._scheduledDuration += extension;
+                    }
+                    return true;
+                }
+
+                this.scheduledTimer.clear();
+
+                var that = this,
+                    onScreenTime = this.scheduledTimer.elapsed();
+                if (this._scheduledDuration + extension > this._maxDuration) {
+                    extension = this._maxDuration - onScreenTime;
+                    this._scheduledDuration = this._maxDuration;
+                } else {
+                    this._scheduledDuration += extension;
+                    extension = this._scheduledDuration - onScreenTime;
+                }
+
+                this.scheduledTimer = this.setTimeout(function () { return that.emitCompleted(); }, extension);
+
+                return true;
+            },
+            emitCompleted: function () {
+                this.trigger('completed');
+                return false;
+            },
+            emitTimeOut: function () {
+                this.trigger('timedout');
+                return false;
+            },
+            setTimeout: function (callback, milliseconds) {
+                if (!_.isFunction(callback) || !_.isNumber(milliseconds)) {
+                    return undefined;
+                }
+                var exports = function () {},
+                    startTime = Timer.getNow(),
+                    stopTime,
+                    updateTimeout,
+                    updateFrequency = Math.min(Math.ceil(milliseconds / 10), 1000), // every x milliseconds
+                    cleared = false,
+                    timeout;
+                updateTimeout = function () {
+                    if (!cleared) {
+                        var nextInterval = Math.min(updateFrequency, milliseconds + startTime - Timer.getNow());
+                        if (nextInterval <= 0) {
+                            stopTime = Timer.getNow();
+                            callback.call(this);
+                        } else {
+                            timeout = setTimeout(updateTimeout, nextInterval);
+                        }
+                    }
+                };
+                updateTimeout();
+                exports.elapsed = function () {
+                    return stopTime ? stopTime - startTime : -startTime + Timer.getNow();
+                };
+                exports.clear = function () {
+                    clearTimeout(timeout);
+                    stopTime = Timer.getNow();
+                    cleared = true;
+                    return true;
+                };
+                return exports;
+            },
+            elapsed: function () {
+                return this.timedOutTimer.elapsed();
+            }
+        });
+
+        return Timer;
+    })
+);
